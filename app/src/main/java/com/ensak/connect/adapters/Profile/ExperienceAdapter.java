00f4fe82a91a29fe924dfa29f66962e32a97b4cd
HaytamBarik <152167578+HaytamBarik@users.noreply.github.com>
2023-12-17 21:23:50 +0100

    Experience adapter

--- app/src/main/java/com/ensak/connect/adapters/Profile/ExperienceAdapter.java
+++ app/src/main/java/com/ensak/connect/adapters/Profile/ExperienceAdapter.java
@@ -65,6 +65,27 @@ public class ExperienceAdapter extends RecyclerView.Adapter<ExperienceAdapter.Ex
             descriptionTextView.setText(experience.getDescription());
         }
 
+            periodTextView.setText(formatPeriod(experience.getStartDate(), experience.getEndDate()));
+            descriptionTextView.setText(experience.getDescription());
+        }
+
+        private String formatPeriod(String startDateStr, String endDateStr) {
+            SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.getDefault());
+            SimpleDateFormat outputFormat = new SimpleDateFormat("MMM yyyy", Locale.getDefault());
+
+            try {
+                Date startDate = inputFormat.parse(startDateStr);
+                Date endDate = inputFormat.parse(endDateStr);
+
+                String formattedStartDate = outputFormat.format(startDate);
+                String formattedEndDate = outputFormat.format(endDate);
+
+                return formattedStartDate + " - " + formattedEndDate;
+            } catch (ParseException e) {
+                e.printStackTrace();
+                return "Invalid dates";
+            }
+        }
     }
 }
 
