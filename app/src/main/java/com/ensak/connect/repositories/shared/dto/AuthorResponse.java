8e6a7d78db9e7ad2d4a169145b88c7f17e1a781e
MouadBNL <mouadbnl.2k01@gmail.com>
2023-12-19 15:21:08 +0100

    feat: added create question with request to backend

---
+++ app/src/main/java/com/ensak/connect/repositories/shared/dto/AuthorResponse.java
@@ -0,0 +1,7 @@
+package com.ensak.connect.repositories.shared.dto;
+
+public class AuthorResponse {
+    private Integer id;
+    private String email;
+    private String role;
+}
--- app/src/main/java/com/ensak/connect/view/create_question_screen/CreateQuestionActivity.java
+++ app/src/main/java/com/ensak/connect/view/create_question_screen/CreateQuestionActivity.java
@@ -1,11 +1,14 @@
 package com.ensak.connect.view.create_question_screen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.ensak.connect.R;
 import com.ensak.connect.databinding.ActivityCreateQuestionBinding;
+import com.ensak.connect.view.LoadingScreen.LoadingViewModel;
 
 public class CreateQuestionActivity extends AppCompatActivity {
     private final String TAG = getClass().getSimpleName();
@@ -18,8 +21,41 @@ public class CreateQuestionActivity extends AppCompatActivity {
         binding = ActivityCreateQuestionBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        initViewModel();
+
         binding.btnCancel.setOnClickListener(v -> {
             finish();
         });
+
+        binding.btnCreate.setOnClickListener(v -> {
+            createQuestionPost();
+        });
+    }
+
+    private void initViewModel() {
+        createQuestionViewModel = new ViewModelProvider(this, new ViewModelProvider.AndroidViewModelFactory(getApplication()))
+                .get(CreateQuestionViewModel.class);
+
+        createQuestionViewModel.getIsSuccess().observe(this, success -> {
+            if(success) {
+                finish();
+            }
+        });
+
+        createQuestionViewModel.getSuccessMessage().observe(this, msg -> {
+            if(msg.length() == 0) return;
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+        });
+
+        createQuestionViewModel.getErrorMessage().observe(this, msg -> {
+            if(msg.length() == 0) return;
+            Toast.makeText(this, "Error: " + msg, Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void createQuestionPost() {
+        String question = binding.txtQuestion.getText().toString().trim();
+        String tags = binding.txtTags.getText().toString().trim();
+        createQuestionViewModel.createQuestionPost(question, tags);
     }
 }
