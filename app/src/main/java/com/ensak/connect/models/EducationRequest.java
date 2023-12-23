2ac9b8cbc611f3a02db5548e8462247e4d3c36c3
HaytamBarik <152167578+HaytamBarik@users.noreply.github.com>
2023-12-23 14:04:50 +0100

    Integration of post apis experience and experience for the profile

---
+++ app/src/main/java/com/ensak/connect/models/EducationRequest.java
@@ -0,0 +1,61 @@
+package com.ensak.connect.models;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class EducationRequest {
+    private String field;
+    private String degree;
+    private String school;
+    private String startDate;
+    private String endDate;
+    private String description;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
---
+++ app/src/main/java/com/ensak/connect/models/ExperienceRequest.java
@@ -0,0 +1,72 @@
+package com.ensak.connect.models;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+public class ExperienceRequest {
+    private String positionTitle;
+    private String contractType;
+    private String companyName;
+
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    private String location;
+    private String startDate;
+    private String endDate;
+    private String description;
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public String getContractType() {
+        return contractType;
+    }
+
+    public void setContractType(String contractType) {
+        this.contractType = contractType;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
