7c7d9e838288ba3dbecb5dce9c0bf3b72574d20e
MouadBNL <mouadbnl.2k01@gmail.com>
2023-12-24 17:17:15 +0100

    refact: change LoginVM to use auth repository

--- app/src/main/java/com/ensak/connect/view/login/LoginViewModel.java
+++ app/src/main/java/com/ensak/connect/view/login/LoginViewModel.java
@@ -11,48 +11,28 @@ import com.ensak.connect.core.SessionManager;
 import com.ensak.connect.models.LoginRequest;
 import com.ensak.connect.reponse.LoginResponse;
 import com.ensak.connect.repositories.LoginRepository;
+import com.ensak.connect.repositories.auth.AuthRepository;
 import com.ensak.connect.retrofit.ApiRequest;
 import com.ensak.connect.retrofit.RetrofitRequest;
 
 
 public class LoginViewModel extends AndroidViewModel {
 
-    private final LoginRepository loginRepository;
-    private final MutableLiveData<LoginResponse> loginResponseLiveData;
-    private final SessionManager sessionManager;
+    private AuthRepository authRepository;
+    private MutableLiveData<Boolean> isLoading = new MutableLiveData<>(false);
+    private MutableLiveData<Boolean> hasLoggedIn = new MutableLiveData<>(false);
+    private SessionManager sessionManager;
 
 
     public LoginViewModel(@NonNull Application application) {
         super(application);
-        ApiRequest apiRequest = RetrofitRequest.getRetrofitInstance(application).create(ApiRequest.class);
-        loginRepository = new LoginRepository(apiRequest);
-        loginResponseLiveData = new MutableLiveData<>();
+        authRepository = new AuthRepository(application);
         sessionManager = new SessionManager(application);
     }
 
-    public LiveData<LoginResponse> getLoginResponseLiveData() {
-        return loginResponseLiveData;
-    }
-
     public void login(String email, String password) {
-        LoginRequest loginRequest = new LoginRequest();
-        loginRequest.setEmail(email);
-        loginRequest.setPassword(password);
-
-        LiveData<LoginResponse> responseLiveData = loginRepository.loginUser(loginRequest);
-        Observer<LoginResponse> observer = new Observer<LoginResponse>() {
-            @Override
-            public void onChanged(LoginResponse loginResponse) {
-                loginResponseLiveData.setValue(loginResponse);
-                if (loginResponse != null && loginResponse.getToken() != null) {
-                    sessionManager.createSession(loginResponse.getToken());
-                }
-                // Remove the observer to prevent multiple subscriptions
-                responseLiveData.removeObserver(this);
-            }
-        };
-
-        responseLiveData.observeForever(observer);
+        isLoading.setValue(true);
+        // Send login request using auth repository
     }
 
 }
