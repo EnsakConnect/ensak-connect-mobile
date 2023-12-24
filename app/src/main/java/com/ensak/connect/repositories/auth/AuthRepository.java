f11743be3d9e0aabb4ed6e25acd5d3314aa0270d
MouadBNL <mouadbnl.2k01@gmail.com>
2023-12-24 17:23:41 +0100

    feat: added login to auth repository

--- app/src/main/java/com/ensak/connect/repositories/auth/AuthRepository.java
+++ app/src/main/java/com/ensak/connect/repositories/auth/AuthRepository.java
@@ -5,6 +5,8 @@ import android.util.Log;
 
 import com.ensak.connect.repositories.RepositoryCallBack;
 import com.ensak.connect.repositories.auth.remote.AuthApi;
+import com.ensak.connect.repositories.auth.remote.dto.LoginRequest;
+import com.ensak.connect.repositories.auth.remote.dto.LoginResponse;
 import com.ensak.connect.repositories.auth.remote.dto.UserResponse;
 import com.ensak.connect.retrofit.RetrofitRequest;
 
@@ -38,5 +40,22 @@ public class AuthRepository {
         });
     }
 
+    public void login(LoginRequest request, RepositoryCallBack<LoginResponse> callBack) {
+        api.login(request).enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                if(! response.isSuccessful() || response.body() == null) {
+                    callBack.onFailure(new Exception("Request Failed, code: " + response.code()));
+                    return;
+                }
+                Log.d(TAG, "onResponse: " + response);
+                callBack.onSuccess(response.body());
+            }
 
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+                callBack.onFailure(t);
+            }
+        });
+    }
 }
