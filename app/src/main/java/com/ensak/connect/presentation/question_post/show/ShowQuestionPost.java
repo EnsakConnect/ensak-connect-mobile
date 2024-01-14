6fdb9ec8b21edd40ea40f8219b55311237dcfa6a
MouadBNL <mouadbnl.2k01@gmail.com>
2024-01-14 10:14:51 +0100

    feat: added answer design

---
+++ app/src/main/java/com/ensak/connect/presentation/question_post/show/ShowQuestionPost.java
@@ -0,0 +1,16 @@
+package com.ensak.connect.presentation.question_post.show;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ensak.connect.R;
+
+public class ShowQuestionPost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question_post_show_activity);
+    }
+}
