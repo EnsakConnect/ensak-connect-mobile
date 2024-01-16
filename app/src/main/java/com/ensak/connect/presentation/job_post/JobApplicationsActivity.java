37244df463ca6d6ea5b9016c820a7335644f7320
TheMadDrunk <iophamza2@gmail.com>
2024-01-16 04:55:11 +0100

    job application ui

---
+++ app/src/main/java/com/ensak/connect/presentation/job_post/JobApplicationsActivity.java
@@ -0,0 +1,16 @@
+package com.ensak.connect.presentation.job_post;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ensak.connect.R;
+
+public class JobApplicationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.job_applications_activity);
+    }
+}
