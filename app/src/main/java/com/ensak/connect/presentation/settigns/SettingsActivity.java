d91672021e89353b84a1feb445647f4c98d156c3
HaytamBarik <152167578+HaytamBarik@users.noreply.github.com>
2024-01-16 11:03:57 +0100

    Nav bar fix

--- app/src/main/java/com/ensak/connect/presentation/settigns/SettingsActivity.java
+++ app/src/main/java/com/ensak/connect/presentation/settigns/SettingsActivity.java
@@ -6,23 +6,30 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
 import com.ensak.connect.R;
+import com.ensak.connect.databinding.ProfileActivityBinding;
+import com.ensak.connect.databinding.SettingsActivityBinding;
 
 public class SettingsActivity extends AppCompatActivity {
 
+
+    SettingsActivityBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.settings_activity);
+        binding = SettingsActivityBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
                     .replace(R.id.settings, new SettingsFragment())
                     .commit();
         }
+
+        setSupportActionBar(binding.toolbar);
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        binding.toolbar.setNavigationOnClickListener(v -> finish());
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
